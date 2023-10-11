886d648332d42b4ad184e4cae7797e7384f8ee3d
20230824 <nike52book@gmail.com>
2023-10-11 12:03:46 +0900

    バリデーション追加

--- app/models/todaygoal.rb
+++ app/models/todaygoal.rb
@@ -1,4 +1,9 @@
 class Todaygoal < ApplicationRecord
+
+  validates :object, presence: true
+  validates :plan,   presence: true
+  validates :action, presence: true
+ 
   belongs_to :user
   belongs_to :vision
 end
